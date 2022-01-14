2ec0f9d6fdd73e4c2a3e8f23bbe43c9d1ca3988d
ValleLuca <valle.lucaAb@outlook.it>
2022-01-14 16:20:23 +0100

    Aggiunta js nel dom

--- js/main.js
+++ js/main.js
@@ -15,7 +15,40 @@
  * ARRAY
  */
 
-const items = [
+var app = new Vue({
+    el: '#container',
+    data: {
+        boxProva: [
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                descr: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                descr: 'Lorem ipsum.'
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                descr: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                descr: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.' 
+            },
+            {
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                descr: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam'
+            }
+        ]
+    }
+  })
+
+/*const items = [
     'img/01.jpg',
     'img/02.jpg',
     'img/03.jpg',
@@ -38,6 +71,7 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+*/
 
 const itemsRef = document.getElementsByClassName('items')[0];
 const thumbsRef = document.getElementsByClassName('thumbs')[0];
