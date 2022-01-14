753cc6efdace7a40f29878bda6b87a2e7108a64a
ValleLuca <valle.lucaAb@outlook.it>
2022-01-14 15:52:16 +0100

    Aggiunto bootstrap per creare struttura dom

--- data.js
+++ data.js
@@ -1,29 +1,6 @@
 var app = new Vue({
     el: '#container',
     data: {
-        box: {
-            boxImage: [
-                {image: 'img/01.jpg' },
-                {image: 'img/02.jpg' },
-                {image: 'img/03.jpg' },
-                {image: 'img/04.jpg' },
-                {image: 'img/05.jpg' }
-            ],
-            boxTitle: [
-                {title: 'Svezia' },
-                {title: 'Svizzera' },
-                {title: 'Gran Bretagna' },
-                {title: 'Germania' },
-                {title: 'Paradise' }
-            ],
-            boxDescr: [
-                {descr: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.' },
-                {descr: 'Lorem ipsum.' },
-                {descr: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.' },
-                {descr: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.' },
-                {descr: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam' }
-            ]
-        },
         boxProva: [
             {
                 image: 'img/01.jpg',
